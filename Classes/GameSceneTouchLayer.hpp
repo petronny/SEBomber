2779411553d14ab84cd94eee7e61c08d461b7c3b
黎静北 <jingbei.li@yahoo.com>
2013-09-29 16:17:25 +0800

    变更文件结构

--- Classes/GameSceneTouchLayer.hpp
+++ Classes/GameSceneTouchLayer.hpp
@@ -2,18 +2,39 @@
 #define __GAME_SCENE_TOUCH_LAYER__
 #include "cocos2d.h"
 USING_NS_CC;
-class GameSceneTouchLayer : public CCLayer
+class GameSceneTouchLayer : public CCLayer,public CCTouchDelegate
  {
- public:
-	CC_SYNTHESIZE(TouchLayerDelegate*,m_pDelegate,Delegate);
 public:
- bool init();
- void ccTouchesBegan(CCSet *pTouches, CCEvent *pEvent);
- void ccTouchesMoved(CCSet *pTouches, CCEvent *pEvent);
- void ccTouchesEnded(CCSet *pTouches, CCEvent *pEvent);
- void ccTouchesCancelled(CCSet *pTouches, CCEvent *pEvent);
- void onExit();
-
+  bool init();
+  bool ccTouchBegan(CCTouch* pTouch, CCEvent* pEvent);
+  void ccTouchMoved(CCTouch* pTouch, CCEvent* pEvent);
+  void ccTouchEnded(CCTouch* pTouch, CCEvent* pEvent);
+  void ccTouchCancelled(CCTouch* pTouch, CCEvent* pEvent);
+  void registerWithTouchDispatcher();
 };
-
+bool GameSceneTouchLayer::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !CCLayer::init() )
+    {
+        return false;
+    }
+    this->setTouchEnabled(true);
+    return true;
+}
+bool GameSceneTouchLayer::ccTouchBegan(CCTouch *pTouch, CCEvent *pEvent){
+	return false;
+}
+void GameSceneTouchLayer::ccTouchCancelled(CCTouch *pTouch, CCEvent *pEvent){
+}
+void GameSceneTouchLayer::ccTouchMoved(CCTouch *pTouch, CCEvent *pEvent){
+}
+void GameSceneTouchLayer::ccTouchEnded(CCTouch *pTouch, CCEvent *pEvent){
+}
+void GameSceneTouchLayer::registerWithTouchDispatcher()
+{
+    CCDirector::sharedDirector()->getTouchDispatcher()->addTargetedDelegate(this, 0, true);
+    CCLayer::registerWithTouchDispatcher();
+}
 #endif
