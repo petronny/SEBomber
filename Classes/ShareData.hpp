deba8692003fa364707b8df78066961aa597fc83
张晋玮 <952999351@qq.com>
2013-10-06 10:16:30 +0800

    跳过登陆界面进入后续，便于调试

--- Classes/ShareData.hpp
+++ Classes/ShareData.hpp
@@ -7,6 +7,13 @@ public :
 	static int face;
 	static int rank;
 	static int magicBubbleNum;
+	ShareData(){
+		username[0]='a';
+		userid=1;
+		face=1;
+		rank=1;
+		magicBubbleNum=1;
+	}
 };
 char ShareData::username[20];
 int ShareData::userid;
